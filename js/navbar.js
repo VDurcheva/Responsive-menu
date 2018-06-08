ab7e3eb33c8d57c6a402141652f037c33875cce3
Vyara Durcheva <34923098+VDurcheva@users.noreply.github.com>
2018-06-08 13:50:47 +0300

    Add files via upload

--- js/navbar.js
+++ js/navbar.js
@@ -1,1 +1,41 @@
+(function ($) {
+    'use strict';
 
+    var $window = $(window);
+ 
+    $window.on('scroll', function () {
+        if ($window.scrollTop() > 20) {
+            $('.header-area').addClass('sticky');
+        } else {
+            $('.header-area').removeClass('sticky');
+        }
+    });
+
+    
+
+    var search = document.getElementById('search'),
+        searchWrapper = document.getElementById('search-wrapper'),
+        closeIcon = document.getElementById('close-icon');
+    search.onfocus = function () {
+        searchWrapper.classList.add('search-expanded');
+        this.addEventListener('transitionend', function () {
+            closeIcon.style.display = 'block';
+        });
+    }
+    search.onblur = function () {
+        searchWrapper.classList.remove('search-expanded');
+        closeIcon.classList.add('closing');
+        this.addEventListener('transitionend', function () {
+            closeIcon.classList.remove('closing');
+            closeIcon.style.display = 'none';
+        });
+    }
+    closeIcon.onclick = function () {
+        this.classList.add('closing');
+        document.activeElement.blur();
+        setTimeout(function () {
+            closeIcon.classList.remove('closing');
+        }, 1000);
+    }
+
+})(jQuery);
